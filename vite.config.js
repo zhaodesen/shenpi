79fb5d8f3e57f05ed15c58d59b882fd9d828f979
zhaodesen <1026108026@qq.com>
2026-06-17 11:15:07 +0800

    feat: github page

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const base = process.env.GITHUB_ACTIONS && repoName ? `/${repoName}/` : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [vue()],
   resolve: {
     alias: {
